89cc08d038bb4cb210fa5933682978a9be6cb0e5
jeffbrn <jeff.brn@gmail.com>
2020-08-31 22:40:30 +0000

    fix timescale deploy

--- devops/timescale/1-up.sh
+++ devops/timescale/1-up.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p /tmp/ts_k3dvol
 U=`logname`
 k3d cluster create ts -p 5432:31432@loadbalancer --volume /tmp/ts_k3dvol:/tmp/ts_k3dvol --agents 1
 echo "Changing kube config owner to ${U}"
 chown $U ~/.kube/config
 kubectl apply -f vols.yaml
 kubectl create -f timescale-deployment.yaml
+watch -n 1 kubectl get po
